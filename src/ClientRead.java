bf2c21d1a21987005fc68e81be6d5cbfc663b847
AleFeri <50620354+AleFeri@users.noreply.github.com>
2020-10-24 22:57:39 +0200

    Initial commit

---
+++ src/ClientRead.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClientRead extends Thread {
+    Client client;
+    String stringFromServer;
+    BufferedReader inFromServer;
+    
+    //Costruttore
+    public ClientRead(Client client) {
+        this.client = client;
+    }
+    
+    //metodi
+    public void run() {
+        try {
+            inFromServer = new BufferedReader(new InputStreamReader(client.mySocket.getInputStream()));
+        } catch (IOException ex) {
+            System.out.println("Error: IOInstrauration");
+        }
+
+        for(;;) {
+            try {
+                stringFromServer = inFromServer.readLine();
+                //System.out.println(stringFromServer);
+                client.println(stringFromServer);
+            } catch (IOException ex) {
+                System.out.println("Error: Ricezione del messaggio");
+            }
+        }
+    }
+}
---
+++ src/ServerThread.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerThread extends Thread {
+    Server server;
+    UserData user;
+    String stringFromUser;
+    BufferedReader inFromClient;
+    DataOutputStream outToClient;
+    
+    
+    //Costruttore
+    public ServerThread(Server server, UserData user) {
+        this.server = server;
+        this.user = user;
+    }
+    
+    //Metodi
+    public void run() {
+        try {
+            exe();
+        }
+        catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+    public void exe() {
+        try {
+            inFromClient = new BufferedReader(new InputStreamReader(user.getUserSocket().getInputStream()));
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        
+        for(;;) {
+            try {
+                stringFromUser = inFromClient.readLine();
+
+                String reciver = UserArray.searchUser(stringFromUser);
+
+                if(!reciver.equals(user.getUsername())) {
+                    Socket reciverSocket = UserArray.getUserSocket(reciver);
+                    outToClient = new DataOutputStream(reciverSocket.getOutputStream());
+
+                    stringFromUser = user.getUsername() + "|" + stringFromUser.substring(stringFromUser.indexOf("|") + 1);
+
+                    outToClient.writeBytes(stringFromUser + '\n');
+                }
+                else {
+                    outToClient = new DataOutputStream(user.getUserSocket().getOutputStream());
+
+                    outToClient.writeBytes("Server| Self Writing is not allowed!\n");
+                }
+
+                close();
+
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+    public void close() {
+        String str = stringFromUser.substring(stringFromUser.indexOf('|') + 1);
+
+        if(str.equals(" EXIT"))
+            this.interrupt();
+    }
+}
